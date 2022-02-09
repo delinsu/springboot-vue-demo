f32e1a97d60360027da5d56d275df2d598dd74ea
shtahal <phshlcmhcscl159>
2022-02-09 20:17:51 +0800

    Initial commit

---
+++ vue/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div style="height: 60px; line-height: 60px; border-bottom: 1px solid #ccc; display: flex">
+    <div style="width: 300px; padding-left: 30px; font-weight: bold; color: dodgerblue">后台管理</div>
+    <div style="flex: 1"></div>
+    <div style="width: 100px">
+      <el-dropdown>
+        张三
+        <span class="el-dropdown-link">
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>个人信息</el-dropdown-item>
+            <el-dropdown-item>退出系统</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { ArrowDown } from '@element-plus/icons-vue'
+
+  export default {
+    components: {
+      ArrowDown
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,79 @@
+<template>
+  <div style="width: 100%; padding: 10px">
+    <div style="display: flex">
+      <div style="width: 300px; margin: 10px 0">
+        <el-row>
+          <el-button type="primary">新增</el-button>
+          <el-button type="primary">导入</el-button>
+          <el-button type="primary">导出</el-button>
+        </el-row>
+      </div>
+      <div style="flex: 1"></div>
+      <div style="width: 300px; margin: 10px 0">
+        <el-input v-model="search" placeholder="Please input" style="width: 70%"/>
+        <el-button type="primary" style="margin-left: 8px">搜索</el-button>
+      </div>
+    </div>
+    <div style="display: flex">
+      <el-table :data="tableData" border stripe>
+        <el-table-column prop="date" label="Date" sortable width="180" />
+        <el-table-column prop="name" label="Name" width="180" />
+        <el-table-column prop="address" label="Address" width="180" />
+        <el-table-column label="Operations" width="180">
+          <template #default>
+            <el-button type="primary" size="small" @click="handleEdit">编辑</el-button>
+            <el-popconfirm title="确定删除?">
+              <template #reference>
+                <el-button type="danger" size="small">删除</el-button>
+              </template>
+            </el-popconfirm>
+          </template>
+        </el-table-column>
+      </el-table>
+    </div>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+
+  },
+  data() {
+    return {
+      search: "",
+      tableData: [
+        {
+          date: '2016-05-03',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-02',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-04',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+        {
+          date: '2016-05-01',
+          name: 'Tom',
+          address: 'No. 189, Grove St, Los Angeles',
+        },
+      ]
+    }
+  },
+  methods: {
+    handleEdit() {
+
+    }
+  }
+}
+</script>
